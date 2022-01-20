7692baa44d228c25b550c16899fb1f9dbedd5ec3
AndreHabib <andre.habib.017@ufrn.edu.br>
2022-01-20 12:27:39 -0300

    Adicionar arquivos de projeto.

---
+++ InAndOut/Data/AplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using InAndOut.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InAndOut.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<AplicationDbContext> options ) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Item> Items{ get; set; }
+    }
+}
---
+++ InAndOut/Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {   
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Expense")]
+        [Required]
+        public string ExpenseName { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage ="Amount must be greather than 0!")]
+        public int Amount { get; set; }
+    }
+}
